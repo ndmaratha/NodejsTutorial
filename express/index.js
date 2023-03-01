4b60d69080cfa860cab51c787f5d2441fab8aa21
Nayan Desai <ndmaratha01@gmail.com>
2023-03-01 13:07:31 +0530

    Rest api chapter Done

--- express/index.js
+++ express/index.js
@@ -5,9 +5,39 @@ const products = data.products;
 
 const express = require("express");
 const server = express();
-server.get("/", (req, res) => {
+
+//built in middleware
+server.use(express.json())
+server.use(express.static('public'))
+//server.use(express)
+
+//middleware
+server.use((req, res, next) => {
+	console.log(req.method, req.hostname);
+	next();
+});
+const auth = (req,res, next) => {
+	//console.log(req.query);
+	if (req.body.password == 123) {
+		next();
+	} else {
+		res.send("unauthorised");
+	}
+};
+
+server.get("/", auth,(req, res) => {
 	res.send("Radhey Radhey");
 });
+server.post("/", (req, res) => {
+	res.send("post");
+});
+server.patch("/", (req, res) => {
+	res.send("patch");
+});
+server.delete("/", (req, res) => {
+	res.send("delete");
+});
+
 server.listen(8080, () => {
 	console.log("server working successfully");
 });
